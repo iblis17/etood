d4ba244aba7c9ba00ac62a6cf584e14d6b3b6de3
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-01-03 22:59:02 +0800

    new module: supervisor and client

---
+++ src/client.erl
@@ -0,0 +1,13 @@
+-module(client).
+
+-compile(export_all).
+
+
+%% a simple client to receive all message
+listen(Timeout) ->
+  receive
+    Msg = {done, _Name, _Desc} ->
+      [Msg|listen(0)]  %% no last call optimization
+  after Timeout * 1000 ->
+    []
+  end.
---
+++ src/sup.erl
@@ -0,0 +1,34 @@
+-module(sup).
+
+-compile(export_all).
+
+
+start() ->
+  register(?MODULE, Pid=spawn(?MODULE, init, [])),
+  Pid.
+
+
+start_link() ->
+  register(?MODULE, Pid=spawn_link(?MODULE, init, [])),
+  Pid.
+
+
+init() ->
+  process_flag(trap_exit, true),
+  loop().
+
+
+loop() ->
+  Pid = tkserv:start_link(),
+  receive
+    {'EXIT', _From, shutdown} ->
+      exit(shutdown);
+    {'EXIT', Pid, Reason} ->
+      io:format("task server ~p crashed: ~p~nrestart it ~n", [Pid, Reason]),
+      loop()
+  end.
+
+
+terminate() ->
+  true = exit(whereis(?MODULE), shutdown),
+  ok.
