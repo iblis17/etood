a79df214cd7832dbb115f9aab3b4e1709245ac50
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-01-05 15:49:03 +0800

    [task] fit task module into gen_server

--- src/etood_task.erl
+++ src/etood_task.erl
@@ -1,44 +1,81 @@
 -module(etood_task).
 
+-behavior(gen_server).
+
 -compile(export_all).
 
 -record(state, {server, name="", expire=[0]}).
 
 
+%% public APIs
+
 start(Name, Exp) ->
-  spawn(?MODULE, init, [self(), Name, Exp]).
+  gen_server:start(?MODULE, {self(), Name, Exp}, []).
 
 
 start_link(Name, Exp) ->
-  spawn_link(?MODULE, init, [self(), Name, Exp]).
+  gen_server:start_link(?MODULE, {self(), Name, Exp}, []).
 
 
-init(Server, Name, Exp) ->
-  loop(#state{server=Server, name=Name, expire=datetime_expire(Exp)}).
+cancel(Pid) ->
+  gen_server:call(Pid, cacnel).
 
 
-loop(State=#state{server=Server, name=N, expire=[Exp|Next]}) ->
-  receive
-    {Server, Ref, cancel} ->
-      Server ! {Ref, ok};
+renew(Pid, Exp) ->
+  gen_server:call(Pid, {renew, Exp}).
 
-    {Server, Ref, {renew, NewExp}} ->
-      NewState = State#state{expire=datetime_expire(NewExp)},
-      Server ! {Ref, ok},
-      loop(NewState)
 
-  after Exp * 1000 ->
-    case Next of
-      [] -> Server ! {done, N};
-      _ -> loop(State#state{expire=Next})
-    end
-  end.
+%% gen_server functions
 
+init({Server, Name, Exp}) ->
+  [Timeout|NextExp] = datetime_expire(Exp),
+  {ok, #state{server=Server, name=Name, expire=NextExp}, Timeout}.
+
+
+handle_call(cacnel, _From, State) ->
+  {stop, normal, ok, State};
+
+handle_call({renew, NewExp}, _From, State) ->
+  NewState = State#state{expire=datetime_expire(NewExp)},
+  {reply, ok, NewState}.
+
+
+handle_info(timeout, State=#state{server=Srv, name=N, expire=Exp}) ->
+  if
+    Exp =/= [] ->
+      {noreply, State#state{expire=tl(Exp)}, hd(Exp)};
+    Exp =:= [] ->
+      Srv ! {done, N},
+      {stop, normal, State}
+  end;
+
+handle_info(Msg, State) ->
+  io:format("unknown message: ~p~n", [Msg]),
+  {noreply, State}.
+
+
+handle_cast(Msg, State) ->
+  io:format("unknown cast operation: ~p~n", [Msg]),
+  {noreply, State}.
+
+
+code_change(_OldVer, State, _Extra) ->
+  %% current no upgrade plan
+  {ok, State}.
+
+
+terminate(_Reason, _State) ->
+  %% TODO: maybe write log to db
+  ok.
+
+
+%% private functions
 
 %% erlang has a limitation of time out seconds: it should =< 50 days
 split_expire(N) when is_number(N) ->
-  Limit = 49 * 86400,  %% 49 days
-  [N rem Limit | lists:duplicate(N div Limit, Limit)].
+  Limit = 49 * 86400 * 1000,  %% 49 days
+  [(N rem Limit) * 1000 | lists:duplicate(N div Limit, Limit)].
+
 
 datetime_expire(DateTime={{_, _, _}, {_, _, _}}) ->
   Now = calendar:local_time(),
@@ -51,22 +88,3 @@ datetime_expire(DateTime={{_, _, _}, {_, _, _}}) ->
       Delta =< 0 -> 0
     end,
   split_expire(Secs).
-
-
-cancel(Pid) ->
-  Ref = erlang:monitor(process, Pid),
-  Pid ! {self(), Ref, cancel},
-  receive
-    {Ref, ok} ->
-      erlang:demonitor(Ref, [flush]),
-      ok;
-    {'DOWN', Ref, process, Pid, _Reason} -> ok
-  end.
-
-
-renew(Pid, Exp) ->
-  Ref = make_ref(),
-  Pid ! {self(), Ref, {renew, Exp}},
-  receive
-    {Ref, ok} -> ok
-  end.
